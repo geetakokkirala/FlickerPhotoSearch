ed85b67812e6e7aa318bbb4ebf8675b08af0d913
geetakokkirala <geetakpb@gmail.com>
2022-02-28 16:00:43 +0530

    add image downloader and cacher functionality

---
+++ FlickerPhotoSearch/ImageDownloaderCacher/ImageCache.swift
@@ -0,0 +1,28 @@
+//
+//  ImageCache.swift
+//  FlickerPhotoSearch
+//
+//  Created by Geetha on 27/02/22.
+//
+
+import Foundation
+import UIKit
+
+class ImageCache {
+    var cache = NSCache<NSString, UIImage>()
+    
+    func get(forKey: String) -> UIImage? {
+        return cache.object(forKey: NSString(string: forKey))
+    }
+    
+    func set(forKey: String, image: UIImage) {
+        cache.setObject(image, forKey: NSString(string: forKey))
+    }
+}
+
+extension ImageCache {
+    private static var imageCache = ImageCache()
+    static func getImageCache() -> ImageCache {
+        return imageCache
+    }
+}
---
+++ FlickerPhotoSearch/ImageDownloaderCacher/UrlImageView.swift
@@ -0,0 +1,33 @@
+//
+//  UrlImageView.swift
+//  FlickerPhotoSearch
+//
+//  Created by Geetha on 27/02/22.
+//
+
+import SwiftUI
+
+struct UrlImageView: View {
+    
+    
+     @ObservedObject var urlImageModel: UrlImageModel
+       
+       init(urlString: String) {
+           urlImageModel = UrlImageModel(urlString: urlString)
+       }
+       
+       var body: some View {
+           Image(uiImage: urlImageModel.image ?? UrlImageView.defaultImage!)
+               .resizable()
+               .scaledToFit()
+               .frame(width: 100, height: 100)
+       }
+       
+       static var defaultImage = UIImage(named: "NewsIcon")
+}
+
+struct UrlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        UrlImageView(urlString: "")
+    }
+}
